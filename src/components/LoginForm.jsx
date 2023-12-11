b777b53fc983a1c3747d7a66de89dd0097a3ba77
Oskar Picus <oskarpicus@yahoo.com>
2023-12-11 16:33:17 +0200

    Added snackbar

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,17 @@
-import {Box, Button, TextField, Typography} from "@mui/material";
+import {Alert, Box, Button, Snackbar, TextField, Typography} from "@mui/material";
+import {useState} from "react";
 
 export const LoginForm = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     const handleSubmit = () => {
         // todo handle submit
     };
@@ -14,10 +25,15 @@ export const LoginForm = () => {
                 alignItems: 'center',
             }}
         >
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                    Wrong credentials
+                </Alert>
+            </Snackbar>
             <Typography component="h1" variant="h5">
                 Login
             </Typography>
-            <Box onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Box noValidate sx={{ mt: 1 }}>
                 <TextField
                     margin="normal"
                     required
@@ -42,6 +58,7 @@ export const LoginForm = () => {
                     fullWidth
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
+                    onClick={handleSubmit}
                 >
                     Login
                 </Button>
